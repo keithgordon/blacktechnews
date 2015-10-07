fe6122d61c8f0f3962403963af1a4a095e6ab3f5
Keith Gordon <keithg07@gmail.com>
2015-10-06 19:59:15 -0500

    added styling

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 var app = angular.module('blacktechnews', []);
-app.controller('MainCtrl', [
-  '$scope',
-  function($scope){
-    $scope.test = "Hello world";
-
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
-
-    $scope.addPost = function(){
-      if(!$scope.title || $scope.title === '') { return;}
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0
-      });
-      $scope.title = '';
-      $scope.link = '';
+  app.factory('posts', [function(){
+    var o = {
+      posts: [{title: 'Blue1647 Expands Internationally', link: 'http://blue1647.com', upvote:0}]
     };
+    return o;
+  }]);
+  app.controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function($scope, posts){
+      $scope.test = "Hello world";
 
-    $scope.incrementUpVotes = function(post){
-      post.upvotes += 1;
-    };
-   }
-  ])
+      $scope.posts = posts.posts;
+
+      $scope.addPost = function(){
+        if($scope.title === '') { return;}
+        $scope.posts.push({
+          title: $scope.title,
+          link: $scope.link,
+          upvotes: 0
+        });
+        $scope.title = '';
+        $scope.link = '';
+      };
+
+      $scope.incrementUpVotes = function(post){
+        post.upvotes += 1;
+      };
+     }
+    ])
