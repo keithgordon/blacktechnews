1152cba24e0d615b60c1d95b6230f6de8a32bf4b
Keith Gordon <keithg07@gmail.com>
2015-10-08 10:47:50 -0500

    finished angular routing

--- app.js
+++ app.js
@@ -14,33 +14,32 @@ var app = angular.module('blacktechnews', ['ui.router'])
         $urlRouterProvider.otherwise('home');
 
       }])
-  app.factory('posts', [function(){
-    var o = {
-      posts: [{title: 'Blue1647 Expands Internationally', link: 'http://blue1647.com', upvote:0}]
-    };
-    return o;
-  }])
-  app.controller('MainCtrl', [
-    '$scope',
-    'posts',
-    function($scope, posts){
-      $scope.test = "Hello world";
+      app.factory('posts', [function(){
+        var o = {
+          posts: [{title: 'hello', link:'http://blue1647.com', upvotes:3}]
+        };
+        return o;
+      }])
+      app.controller('MainCtrl', [
+        '$scope',
+        'posts',
+        function($scope, posts){
+          $scope.test = 'Hello world';
 
-      $scope.posts = posts.posts;
+          $scope.posts = posts.posts;
 
-      $scope.addPost = function(){
-        if($scope.title === '') { return;}
-        $scope.posts.push({
-          title: $scope.title,
-          link: $scope.link,
-          upvotes: 0
-        });
-        $scope.title = '';
-        $scope.link = '';
-      };
+          $scope.addPost = function() {
+            if($scope.title === '') { return; }
+            $scope.posts.push({
+              title: $scope.title,
+              link: $scope.link,
+              upvotes: 0
+            });
+            $scope.title = '';
+            $scope.link = '';
+          }
 
-      $scope.incrementUpVotes = function(post){
-        post.upvotes += 1;
-      };
-     }
-    ])
+          $scope.incrementUpVotes = function(post){
+            post.upvotes += 1;
+          }
+        }]);
