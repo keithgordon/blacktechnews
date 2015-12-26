271ce2ccc5b67690f81f8f50edd21303cba6a0fd
keithgordon <keithg07@gmail.com>
2015-12-26 11:33:06 -0600

    fixed upvoting functionality  and added author attribute

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -74,7 +74,7 @@ function($stateProvider, $urlRouterProvider) {
   };
 
   o.upvote = function(post) {
-    return $http.put('/posts/' + post._id + '/upvote', {
+    return $http.put('/posts/' + post._id + '/upvote', null, {
       headers: {Authorization: 'Bearer '+auth.getToken()}
     }).success(function(data){
       post.upvotes += 1;
@@ -88,7 +88,7 @@ function($stateProvider, $urlRouterProvider) {
   };
 
   o.upvoteComment = function(post, comment) {
-    return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', {
+    return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
       headers: {Authorization: 'Bearer '+auth.getToken()}
     }).success(function(data){
       comment.upvotes += 1;
