9ba33c4e16460d9e61316c4f7ba7f7430467c6d4
keithgordon <keithg07@gmail.com>
2015-12-26 08:48:33 -0600

    added author attribute

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,7 @@ router.get('/posts', function(req, res, next) {
 
 router.post('/posts', auth, function(req, res, next) {
   var post = new Post(req.body);
+  post.author = req.payload.username;	
 
   post.save(function(err, post){
     if(err){ return next(err); }
